590076f58be6c4f8978236e04493ad122c584be9
mleila <mohamed.laila@mail.mcgill.ca>
2020-03-28 21:49:32 -0400

    [NEW] add clean text module

---
+++ covid/transformations/text_cleaning.py
@@ -0,0 +1,55 @@
+"""
+This module hosts the logic for cleaning raw text data
+"""
+import re
+
+import nltk
+from nltk.corpus import stopwords
+
+
+def remove_punct(text: str):
+    """
+    Remove punctuations using regex rules.
+    """
+    # remove tbc dots
+    text = re.sub('…', '', text)
+    # remove urls
+    text = re.sub('http\S+', '', text, flags=re.MULTILINE)
+    # remove periods and commas, etc.
+    text = (text
+            .replace('.', '')
+            .replace(',', '')
+            .replace(':', '')
+            .replace('#', '')
+            .replace('"', '')
+            .replace("'", '')
+            .replace(")", '')
+            .replace("(", '')
+            )
+    # remove single character words
+    text = ' '.join([word for word in text.split() if len(word) > 1])
+    return text
+
+
+def clean_articles(data: list) -> list:
+    """
+    Full text cleaning pipeline.
+    """
+
+    # lowercase all words
+    data = [sentence.lower() for sentence in data]
+
+    # download english stop words from nltk
+    nltk.download('stopwords')
+
+    # load stop words
+    stop_words = stopwords.words('english')
+
+    # remove stop words
+    data = [' '.join(
+        [w for w in sentence.split() if w not in stop_words]) for sentence in data]
+
+    # remove punctuation
+    data = [remove_punct(sentence) for sentence in data]
+
+    return data
