8246c4ff58ca1030bed24616286562970b32c113
mleila <mohamed.laila@mail.mcgill.ca>
2020-03-31 18:27:32 -0400

    [NEW] add covidash to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 
 scripts = [os.path.join('bin', f) for f in os.listdir('./bin')]
-packages = ['covid']
+packages = ['covid', 'covidash']
 
 setup(name='covid',
       version='0.1',
