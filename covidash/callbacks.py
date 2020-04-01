006704f9a898303c1dc7361f8b562326f6b87c09
mleila <mohamed.laila@mail.mcgill.ca>
2020-03-31 21:05:50 -0400

    [NEW] add tsne plot to the callback

--- covidash/callbacks.py
+++ covidash/callbacks.py
@@ -1,7 +1,8 @@
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from common import make_sentiment_dashboard
+from covidash.common import make_sentiment_dashboard, make_tsne_plot
+from covidash.data import load_embeddings
 from app import app
 
 store = {}
@@ -17,12 +18,14 @@ store['current_button'] = 'tsne'
     ]
 )
 def update_output_div(input_value, tsne_clicks, sentiment_clicks):
+    """
+    """
     tsne_clicks = 0 if tsne_clicks is None else tsne_clicks
     sentiment_clicks = 0 if sentiment_clicks is None else sentiment_clicks
-    print(input_value, tsne_clicks, sentiment_clicks)
     if tsne_clicks > sentiment_clicks:
         tsne_clicks = sentiment_clicks + 1
-        return [html.Div('tsne!')]
+        return make_tsne_plot(topic=input_value)
+
     # default behaviour
     sentiment_clicks = tsne_clicks + 1
     return make_sentiment_dashboard(topic=input_value)
