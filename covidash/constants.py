658ec98c64ed8eb5d85bd309e635ddfebb70c5e2
mleila <mohamed.laila@mail.mcgill.ca>
2020-04-02 20:22:07 -0400

    [FIX] fix typo

--- covidash/constants.py
+++ covidash/constants.py
@@ -3,7 +3,7 @@ This module contains constants defined for the Covid Dash Application.
 """
 
 BANNER_TITLE = 'Covid-19 News Monitor'
-PROJ_TITLE = 'Covid New Monitoring Tool'
+PROJ_TITLE = 'Covid News Monitoring Tool'
 PROJ_DESC = 'This dashboard provides useful visualizations for news articles about a certain topic.'
 WORDCLOUD_TITLE = 'News Artciles Word Cloud'
 
